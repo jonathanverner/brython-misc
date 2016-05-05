06b2dd8d4af7afa9a5a2eebf145761b07de8b05e
Jonathan Verner <jonathan.verner@matfyz.cz>
2016-05-05 17:35:02 +0200

    Add more metadata to Component.

--- app/angular/core/component.py
+++ app/angular/core/component.py
@@ -2,12 +2,14 @@ from browser import window, document
 from .jsdict import JSDict
 
 class Component:
+    METADATA = ['selector','template','templateURL','directives','providers']
     
     def __init__(self):
-        comp = JSDict({
-            'selector':self.selector,
-            'template':self.template
-        })
+        attr_dict = {}
+        for a in self.METADATA:
+            if hasattr(self,a):
+                attr_dict[a]=self.__getattribute__(a)
+        comp = JSDict(attr_dict)
         cls = JSDict({
             'constructor':self.__init__,
         })
