804fb7e08fbcbf4b235b519ba8ccd7383ae0765b
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2016-05-05 10:14:55 +0200

    Misc.

--- test-yield.py
+++ test-yield.py
@@ -1,11 +1,61 @@
 from browser import ajax, window
 
+class PromiseException(Exception):
+    def __init__(self,message):
+        super(PromiseException,self).__init__(message)
+
 class Promise():
-    def __init__(self):
-        pass
+    STATUS_NEW = 0
+    STATUS_INPROGRESS = 1
+    STATUS_FINISHED = 2
+    STATUS_ERROR = 3
+
+    def __init__(self,start_immediately=True,throw_on_error=True):
+        self.throw_on_error = throw_on_error
+        self._status  = Promise.STATUS_NEW
+        self._success_handler = None
+        self._error_handler = None
+        self._result = None
+        if start_immediately:
+            self.start()
     
     def then(self, success_handler, error_handler):
-        pass
+        self._success_handler = success_handler
+        self._error_handler = error_handler
+        if self.status == Promise.STATUS_FINISHED:
+            return self._success_handler(self.result)
+        elif self.status == Promise.STATUS_ERROR:
+            return self._error_handler(self.result)
+
+    def start(self):
+        if self._status == Promise.STATUS_NEW:
+            self._status = Promise.STATUS_INPROGRESS
+            return True
+        else:
+            return False
+
+    @property
+    def result(self):
+        if self._status == Promise.STATUS_FINISHED or self._status == Promise.STATUS_ERROR:
+            return self.result
+        else:
+            raise PromiseException("Not finished")
+
+    @property
+    def status(self):
+        return self._status
+
+    def _finish(self,result,status=Promise.STATUS_FINISHED):
+        self._result = result
+        self._status = status
+        if self._status == Promise.STATUS_FINISHED and self._success_handler:
+            self._success_handler(self.result)
+        elif self._status == Promise.STATUS_ERROR:
+            self._error_handler(self.result)
+
+class Return():
+    def __init__(self,val):
+        self.val = val
     
 class HTTPException(Exception):
     def __init__(self,request):
@@ -16,84 +66,81 @@ class HTTPException(Exception):
 class HTTPRequest(Promise):
     METHOD_POST = 'POST'
     METHOD_GET  = 'GET'
-    STATUS_NEW = 0
-    STATUS_INPROGRESS = 1
-    STATUS_FINISHED = 2
-    STATUS_ERROR = 3
+
     
-    def __init__(self,url,method='GET',data=None,start_immediately=True):
+    def __init__(self,url,method='GET',data=None,**kwargs):
         self._url = url
-        self._success_handler = None
-        self._error_handler = None
-        self._result = None
         self._req = ajax.ajax()
         self._req.bind("complete",self._complete_handler)
-        self._status = HTTPRequest.STATUS_NEW
         self._data = data
         self._method = method
-        if start_immediately:
-            self.start()
+        super(HTTPRequest,self).__init__(**kwargs)
     
     def start(self):
-        if self._status == HTTPRequest.STATUS_NEW:
-            self._status = HTTPRequest.STATUS_INPROGRESS
+        if super(HTTPRequest,self).start():
             self._req.open(self._method,self._url,True)
             self._req.set_header('content-type','application/x-www-form-urlencoded')
             if self._data is None:
                 self._req.send()
             else:
                 self._req.send(self._data)
+            return True
+        else:
+            return False
     
     def _complete_handler(self,req):
         if req.status == 200 or req.status == 0:
-            self._status = HTTPRequest.STATUS_FINISHED
-            self._result = req
-            if self._success_handler:
-                self._success_handler(self._result)
+            self._finish(req)
         else:
-            self._status = HTTPRequest.STATUS_ERROR
-            self._result = HTTPException(req)
-            if self._error_handler:
-                self._error_handler(self._result)
-    
-    def then(self, success_handler, error_handler):
-        self._success_handler = success_handler
-        self._error_handler = error_handler
-        if self._status == HTTPRequest.STATUS_FINISHED:
-            self._success_handler(self._result)
-        elif self._status == HTTPRequest.STATUS_ERROR:
-            self._error_handler(self._result)
+            self._finish(HTTPException(req),Promise.STATUS_ERROR)
 
-def process_async(generator):
+
+def process_async(generator,throw_on_error):
     def run(val):
         try:
             async = generator.send(val)
-            succ,err = process_async(generator)
-            async.then(succ,err)
+            if isinstance(async,Return):
+                return async.val
+            else:
+                succ,err = process_async(generator,async.throw_on_error)
+                return async.then(succ,err)
         except StopIteration:
-            pass
+            return None
     def error(ex):
         try:
-            async = generator.throw(ex)
-            succ,err = process_async(generator)
-            async.then(succ,err)
+            if throw_on_error:
+                async = generator.throw(ex)
+            else:
+                async = generator.send(None)
+            if isinstance(async,Return):
+                return async.val
+            else:
+                succ,err = process_async(generator,async.throw_on_error)
+                return async.then(succ,err)
         except StopIteration:
-            pass
+            return None
             
     return run,error
 
-def run(f,*args,**kwargs):
-    generator = f(*args,**kwargs)
-    async = next(generator,Promise())
-    succ,err = process_async(generator)
-    async.then(succ,err)
 
+def interruptible(f):
+
+    def run(*args,**kwargs):
+        generator = f(*args,**kwargs)
+        async = next(generator)
+        succ,err = process_async(generator)
+        return async.then(succ,err)
+
+    return run
+
+@interruptible
 def wget_urls(urls):
     results = ''
     for url in urls:
-        result = yield HTTPRequest(url)
-        results = results + result.text
-    print(results)
+        result = yield HTTPRequest(url,throw_on_error=False)
+        if result:
+            results = results + result.text
+    yield Return(results)
 
 
 # run(wget_urls,["/media/teaching/alg110006/maze/maze.py","/media/teaching/alg110006/maze/css/style.css"])
