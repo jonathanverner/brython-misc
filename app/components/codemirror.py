4db671ddb1acf66093a9b4f47c312d01ba5c345b
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2016-05-08 23:25:18 +0200

    Add a first try at a codemirror component.

---
+++ app/components/codemirror.py
@@ -0,0 +1,73 @@
+from angular import core as ngcore
+from browser import console, timer
+from jsmodules import jsimport
+import javascript
+
+CodeMirror = jsimport('CodeMirror')
+CodeMirror.js_func = CodeMirror
+CM = javascript.JSConstructor(CodeMirror)
+
+
+class Doc:
+    def __init__(self,val=''):
+        self._val=val
+        self.change = ngcore.Output()
+
+    def on_change(self,change):
+        self._val = change.value
+
+    @property
+    def value(self):
+        return self._val
+
+    @value.setter
+    def value(self,val):
+        self._val = val
+        self.change.pyobj.emit(val)
+
+
+@ngcore.component
+class CodeMirrorComponent(ngcore.Component):
+
+    class ComponentData:
+        selector = 'codemirror'
+        template = '<div #textarea></div>'
+
+        class Outputs:
+            change = ngcore.Output()
+
+        class Inputs:
+            doc = Doc()
+            options = ngcore.JSDict({
+                'mode':ngcore.JSDict({
+                    'name':'python',
+                    'version':3
+                }),
+                'indentUnit':4
+            })
+
+        class ViewElements:
+            textarea = ngcore.ViewChild('textarea')
+
+    def __init__(self):
+        super(CodeMirrorComponent,self).__init__()
+
+    def ngAfterViewInit(self):
+        self.options.value = self.doc.value
+        self.cm = CM(self.textarea.nativeElement,self.options)
+        self._cmdoc = self.cm.getDoc()
+        self._cmdoc.on("change",self._change_handler)
+
+    def subscribe(self):
+        pass
+
+    def _change_handler(self,cmdoc,change):
+        console.log(change)
+        ch = ngcore.JSDict({
+            'value':self._cmdoc.getValue(),
+            'edit':change
+        })
+        console.log(self.change)
+        self.change.pyobj.emit(ch)
+        self.doc.on_change(ch)
+
