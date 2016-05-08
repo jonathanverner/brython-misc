1ba968fe787aa3ec087e1316cf5c4124d4b14d57
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2016-05-08 08:19:39 +0200

    Update the test app.

--- app/app_component.py
+++ app/app_component.py
@@ -1,7 +1,58 @@
 import angular.core 
+from browser import console
 
-class AppComponent(angular.core.Component):
+@angular.core.component
+class ClickComponent(angular.core.Component):
+
+    class ComponentData:
+        selector = 'click-me'
+        template = '<button (click)="click_handler()">{{ title }}</button>{{message}}'
+
+        class Inputs:
+            title = 'Click Me!'
+
     def __init__(self):
-        self.selector = 'my-app'
-        self.template = '<h1>My First Angular 2 App</h1>'
+        super(ClickComponent,self).__init__()
+        console.log("Initializing click")
+        self.message = 'Not clicked'
+
+
+    def click_handler(self,*args,**kwargs):
+        for (k,v) in kwargs.items():
+            console.log(k,":",v)
+        for a in args:
+            console.log(a)
+        self.message = 'Clicked'
+
+@angular.core.component
+class AppComponent(angular.core.Component):
+
+    class ComponentData:
+        selector = 'my-app'
+        template = """
+<h1>My First Angular 2 App: {{ title }} </h1>
+<click-me [title]="'Ahoj'"></click-me><br/>
+Title: <input [(ngModel)] = "title" (keyup)="on_key($event)"/><br/>
+Check: <input type='checkbox' [(ngModel)] = "checked"/><br/>
+Log: {{values }}
+                   """
+        directives = [ClickComponent]
+
+
+    def __init__(self):
         super(AppComponent,self).__init__()
+        console.log("Initializing App")
+        self.title='My great Title'
+        self.values = ""
+        self.checked=True
+
+    def on_key(self,event):
+        self.values = event.target.value
+        pass
+
+
+
+
+
+
+
--- app/main.py
+++ app/main.py
@@ -1,16 +1,14 @@
 import sys
 sys.path.append('/app')
 
-from browser import window
-from app_component import AppComponent
-from angular.core import JSDict
-
+from browser import window, console
+from app_component import AppComponent, ClickComponent
+from angular.core import JSDict, component
 
 def main():
-    app = AppComponent()
-    app.register()
     window.app = JSDict({
-        'AppComponent':app
+        'AppComponent':AppComponent
     })
+
 
 main()
