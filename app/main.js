4277182802036946338b200f0cceb8af5353a7a8
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2016-05-10 11:37:37 +0200

    Indentation fix.

--- app/main.js
+++ app/main.js
@@ -1,18 +1,18 @@
 (function(app) {
-  document.addEventListener('DOMContentLoaded', function() {
-      System.import("primeng/primeng").then(function(primeng) {
-          console.log(primeng);
-          console.log(primeng.Menubar);
-          app.AppComponent =
-            ng.core.Component({
-                selector: 'my-app',
-                template: '<h1>My First Angular 2 App</h1>',
-                directives: [primeng.Menubar]
-            })
-            .Class({
-                constructor: function() {}
-            });
+    document.addEventListener('DOMContentLoaded', function() {
+        System.import("primeng/primeng").then(function(primeng) {
+            console.log(primeng);
+            console.log(primeng.Menubar);
+            app.AppComponent =
+                ng.core.Component({
+                    selector: 'my-app',
+                    template: '<h1>My First Angular 2 App</h1>',
+                    directives: [primeng.Menubar]
+                })
+                .Class({
+                    constructor: function() {}
+                });
             ng.platformBrowserDynamic.bootstrap(app.AppComponent);
-      });
-  });
+        });
+    });
 })(window.app || (window.app = {}));
