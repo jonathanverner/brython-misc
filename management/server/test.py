b6de67049f8aa5146ce33141b54dd1af13377629
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2016-08-02 10:41:02 +0200

    FEATURE(server): Add testing infrastructure based on py.test

--- management/server/test.py
+++ management/server/test.py
@@ -1,10 +1,6 @@
 from fabric.api import task, local
 from management.venv import venv
 
-import pip
-import test
-
-
 @task
-def serve():
-    venv(['python3','server.py'])
+def all():
+    venv(['python','-m', 'pytest', '--pdb','server'])
---
+++ server/tests/settings.py
@@ -0,0 +1,3 @@
+settings = {
+    'temp_dir':'/tmp'
+}
