72581a4b20c0d374a4555aed07824e7e937d1ddb
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2016-08-04 17:36:17 +0200

    DOC: Add documentation to the events module.

--- client/python_packages/lib/events.py
+++ client/python_packages/lib/events.py
@@ -30,11 +30,21 @@ class Event:
 
 
 class EventMixin:
+    """
+        A Mixin class which adds methods to an object to make it possible
+        for it to emit events and for others to bind to its emitted events
+    """
 
     def __init__(self):
         self._event_handlers = {}
 
     def bind(self, event, handler, forward_event=None):
+        """
+           Registers an event handler for event. If @forward_event is provided
+           and handler is an object, registers a handler which emits the
+           event @forward_event on object @handler whenever the current object
+           emits the event @event.
+        """
         if forward_event is not None and isinstance(handler,EventMixin):
             handler = generate_forward_handler(handler,forward_event)
         if event not in self._event_handlers:
@@ -42,6 +52,13 @@ class EventMixin:
         self._event_handlers[event].append(handler)
 
     def unbind(self,event=None,handler=None):
+        """
+           Unregisters event handlers. If @event is None, unregisters
+           ALL handlers for all events. If @event is provided but
+           @handler is None, unregisters all handlers for event @event.
+           Otherwise unregisters only the specified @handler from the
+           event @event.
+        """
         if event is None:
             self._event_handlers = {}
         else:
@@ -51,7 +68,19 @@ class EventMixin:
             else:
                 handlers.remove(handler)
 
-    def emit(self, event, event_data):
+    def emit(self, event, event_data,_forwarded=False):
+        """
+            Emits an envent on the object, calling all event handlers. Each
+            event handler will be passed an object of type Event whose
+            @data attribute will contain @event_data and @targets and @names@
+            attribute will be a list of objects on which the event was called
+            starting from the first one and going up along the forwarding chain
+            (if forward handlers were registered).
+
+            NOTE: _forwarded should NOT be set by the users. If is used
+            internally by forward handlers to indicate that this is a
+            forwarded event.
+        """
         if _forwarded and isinstance(event_data, Event):
             event_data.retarget(self)
             event_data.rename(event)
