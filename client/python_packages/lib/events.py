cd7c87f743a27d4510fa41bc85b829de1bfdf26d
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2016-08-05 00:17:38 +0200

    client.lib.events: Method to add EventMixing via monkey-patching live instances

--- client/python_packages/lib/events.py
+++ client/python_packages/lib/events.py
@@ -25,6 +25,13 @@ class Event:
         return self.targets[-1]
 
 
+def add_event_mixin(obj):
+    """Apply mixins to a class instance after creation"""
+    base_cls = obj.__class__
+    base_cls_name = obj.__class__.__name__
+    obj.__class__ = type(base_cls_name, (EventMixin,base_cls),{})
+    obj._event_handlers = {}
+
 
 class EventMixin:
     """
